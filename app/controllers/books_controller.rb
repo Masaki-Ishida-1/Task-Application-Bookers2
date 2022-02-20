b44111223cabaf572b5e145239a862f9f7bb756d
EC2 Default User <ec2-user@ip-172-31-10-232.ec2.internal>
2022-02-20 15:37:37 +0000

    基本機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,11 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -21,9 +23,25 @@ class BooksController < ApplicationController
     redirect_to book_path(@book.id)
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if
+      @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
   private
   def book_params
     params.require( :book).permit(:title, :body)
   end
-
 end
